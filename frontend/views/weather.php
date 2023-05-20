23959b88971c1ab9b9e0c1bde11e32dfb5d326e8
Anne Thieß <anne.thiess12@gmail.com>
2023-05-20 17:43:01 +0200

    weather fetcher

--- frontend/views/weather.php
+++ frontend/views/weather.php
@@ -6,3 +6,27 @@ Template::header("Your Weather");
     var_dump($this->model);
 
 Template::footer(); ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Weather App</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Weather Information</h1>
+    <div class="weather-info">
+    <?php 
+    // PHP code to fetch and display weather data
+    $json_data = file_get_contents($weather_data);
+    $weather_data = json_decode($json_data, true);
+
+    echo "<p>Temperature: " . $weather_data['main']['temp'] . "°C</p>";
+    echo "<p>Description: " . $weather_data['weather'][0]['description'] . "</p>";
+    ?>
+    </div>
+  </div>
+
+</body>
+</html>
--- weather-data-access/WeatherFetcher.php
+++ weather-data-access/WeatherFetcher.php
@@ -12,38 +12,17 @@ class WeatherFetcher {
     function fetchWeather(){
         // Construct the URL for the API request using the base URL
         $url = $this->base_url . "weather.json";
-        var_dump($url);
+        // var_dump($url);
       
         // Fetch the data from the API using the constructed URL
         $data = file_get_contents($url);
-        var_dump($data);
+        // var_dump($data);
 
       
         // Decode the JSON data
         // The "true" argument to json_decode returns an associative array instead of an object
         $weather = json_decode($data, true);
       
-        // Return the currency codes to the caller
+        // Return the weather to the caller
         return $weather;
-    }
-
-    // Fetches the exchange rate for the specified currency pair
-    // function fetchExchangeRate($from_currency, $to_currency) {
-
-    //     // Construct the URL for the API request using the base URL and currency codes
-    //     $url = "{$this->base_url}currencies/{$from_currency}/{$to_currency}.json";
-      
-    //     // Fetch the data from the API using the constructed URL
-    //     $data = file_get_contents($url);
-      
-    //     // Decode the JSON data and extract the exchange rate value
-    //     // The "true" argument to json_decode returns an associative array instead of an object
-    //     // The [$to_currency] index selects the property of the decoded data, which contains the exchange rate
-    //     $response = json_decode($data, true);
-    //     $exchange_rate = $response[$to_currency];
-      
-    //     // Return the exchange rate value to the caller
-    //     return $exchange_rate;
-    //   }
-  }
-  
+    }}
