3fa3cec9e88f99762262ddbc9a76e711f6a975b6
Anne Thieß <anne.thiess12@gmail.com>
2023-05-22 15:39:45 +0200

    weather data as html

--- frontend/views/weather.php
+++ frontend/views/weather.php
@@ -1,5 +1,6 @@
 <?php
 require_once __DIR__ . "/../Template.php";
+// require_once __DIR__ . "/../../weather-data-access/WeatherFetcher.php";
 
 Template::header("Your Weather"); 
 
@@ -17,13 +18,13 @@ Template::footer(); ?>
   <div class="container">
     <h1>Weather Information</h1>
     <div class="weather-info">
+        <p>LOCATION <?=$this->model["weather"]["data"][0]["city_name"]?></p>
+        <p>Temperature <?=$this->model["weather"]["data"][0]["temp"]?></p>
+        <p>Weather Description <?=$this->model["weather"]["data"][0]["weather"] ["description"]?></p>
     <?php 
-    // PHP code to fetch and display weather data
-    $json_data = file_get_contents($weather_data);
-    $weather_data = json_decode($json_data, true);
-
-    echo "<p>Temperature: " . $weather_data['main']['temp'] . "°C</p>";
-    echo "<p>Description: " . $weather_data['weather'][0]['description'] . "</p>";
+            // <p> Location: " . $weather['data'][0]['city_name'] . "</p>";
+            // $html .= "<p>Temperature: " . $weather['data'][0]['temp'] . " &#8451;</p>";
+            // $html .= "<p>Weather Description: " . $weather['data'][0]['weather']['description'] . "</p>";
     ?>
     </div>
   </div>
