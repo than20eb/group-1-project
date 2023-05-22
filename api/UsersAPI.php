3318df32fb7dd63198603acdcfb35fb0061b9e35
Pauline Wahle <pauline.wahle@web.de>
2023-05-22 13:19:32 +0200

    Update UsersAPI.php

--- api/UsersAPI.php
+++ api/UsersAPI.php
@@ -101,23 +101,23 @@ class UsersAPI extends RestAPI
 
     // Gets the contents of the body and updates the user
     // by sending it to the DB
-    // private function putOne($id)
-    // {
-    //     $user = new UserModel();
+    private function putOne($id)
+    {
+        $user = new UserModel();
 
-    //     $user->username = $this->body["username"];
-    //     $user->password_hash = $this->body["password"];
-    //     $user->location = $this->body["location"];
-    //     $user->premium = $this->body["premium"];
+        $user->username = $this->body["username"];
+        $user->password_hash = $this->body["password"];
+        $user->location = $this->body["location"];
+        $user->premium = $this->body["premium"];
 
-    //     $success = UsersService::updateUserById($id, $user);
+        $success = UsersService::updateUserById($id, $user);
 
-    //     if ($success) {
-    //         $this->ok();
-    //     } else {
-    //         $this->error();
-    //     }
-    // }
+        if ($success) {
+            $this->ok();
+        } else {
+            $this->error();
+        }
+    }
 
     // Deletes the user with the specified ID in the DB
     private function deleteOne($id)
